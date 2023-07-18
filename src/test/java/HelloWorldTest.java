c8847a75e7b8cb3996cab6f79dd298e593aca064
vikenty19 <vikenty.plakhov@testpro.io>
2023-07-18 11:10:22 -0400

    Update HelloWorldTest.java

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -1,9 +1,16 @@
 import org.junit.jupiter.api.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 public class HelloWorldTest {
     @Test
     public void testHelloWorld(){
-        System.out.println("Hello World!");
+       // System.out.println("Hello World!");
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
+
     }
 
 }
