3ebfba056d9b346f46991745c611db1a1943ae40
vikenty19 <vikenty.plakhov@testpro.io>
2023-07-22 12:29:32 -0400

    Update HelloWorldTest.java

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -2,11 +2,17 @@ import org.junit.jupiter.api.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorldTest {
     @Test
-    public void testHelloWorld(){
+    public void testRestAssured(){
        // System.out.println("Hello World!");
+     //   Map<String,String> params = new HashMap<>();
         Response response = RestAssured
+                .given()
+                .queryParam("name","Santa")
                 .get("https://playground.learnqa.ru/api/hello")
                 .andReturn();
         response.prettyPrint();
