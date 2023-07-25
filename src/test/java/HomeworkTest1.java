44a97fcff19f1531b9e0874db0d1fec00391520e
vikenty19 <vikenty.plakhov@testpro.io>
2023-07-25 13:51:53 -0400

    redirect

--- src/test/java/HomeworkTest1.java
+++ src/test/java/HomeworkTest1.java
@@ -1,8 +1,13 @@
 import io.restassured.RestAssured;
+import io.restassured.http.Headers;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class HomeworkTest1 {
     @Test
@@ -15,8 +20,28 @@ public class HomeworkTest1 {
         response.prettyPrint();
         System.out.println(response.getString("messages[1].timestamp"));
         }
+    @Test
+    public void testRestAssured(){
+        Map<String,String> headers = new HashMap<>();
+        headers.put("myHeader1","myValue1");
+        headers.put("myHeader2","myValue2");
 
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+       // int statusCode = response.getStatusCode();
+      //  System.out.println(statusCode);
+        response.prettyPrint();
+    //     Headers responseHeaders = response.getHeaders();
+      //  System.out.println(responseHeaders);
+
+         String locationHeader = response.getHeader("location");
+        System.out.println(locationHeader);
     }
 
 
-
+}
