4fee3dc1c468718fbf530ee56add77b81678a60e
vikenty19 <vikenty.plakhov@testpro.io>
2023-07-28 16:54:43 -0400

    task1

---
+++ src/test/java/Homework3.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class Homework3 {
+    @ParameterizedTest
+    @ValueSource(strings = {"1","12345","12345678910123456"} )
+    public void shortPhraseTest(String givenString){
+        int length = givenString.length();
+        if(length<=15){ givenString = "Name doesn't exist";}
+        String answer = (length > 15) ? givenString : "Name doesn't exist";
+        System.out.println(answer);
+             assertEquals(givenString,answer,"Name doesn't exist");
+
+    }
+}
---
+++ src/test/java/MyTasks.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+public class MyTasks {
+    @Test
+      public void checkWord(){
+        System.out.println(isPalindrome("Aaaa1"));
+      }
+
+    public boolean isPalindrome(String myString) {
+        String myStringReverse = "";
+        for (int i = myString.length() - 1; i >= 0; i--) {
+            myStringReverse = myStringReverse + myString.charAt(i);
+        }
+        if (myString.equalsIgnoreCase(myStringReverse)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+}
