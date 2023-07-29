65926e0d367ee593b70cdc06686767425a3f2a38
vikenty19 <vikenty.plakhov@testpro.io>
2023-07-29 16:47:16 -0400

    Update Homework3.java

--- src/test/java/Homework3.java
+++ src/test/java/Homework3.java
@@ -7,7 +7,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Homework3 {
     @ParameterizedTest
-    @ValueSource(strings = {"1","12345","12345678910123456"} )
+    @ValueSource(strings = {"1","12345","12345678910123456","vasyavasyavasya","vasyavasyavasya1"} )
     public void shortPhraseTest(String givenString){
         int length = givenString.length();
         if(length<=15){ givenString = "Name doesn't exist";}
