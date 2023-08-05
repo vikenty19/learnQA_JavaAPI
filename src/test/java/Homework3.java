f725563a815024796a548b9c2452078678cfe107
vikenty19 <vikenty.plakhov@testpro.io>
2023-08-05 15:59:27 -0400

    Update Homework3.java

--- src/test/java/Homework3.java
+++ src/test/java/Homework3.java
@@ -53,7 +53,10 @@ public class Homework3 {
 
    @ParameterizedTest
    @ValueSource(strings = {"Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
-   "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"})
+   "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+   "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1",
+   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0",
+   "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"})
     public void checkUserAgent(String name){
         Map<String,String> headers = new HashMap<>();
         headers.put("user-agent",name);
@@ -62,10 +65,10 @@ public class Homework3 {
                 .headers(headers)
                 .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
                 .andReturn();
-        response.prettyPrint();
+  //      response.prettyPrint();
         JsonPath jsonPath = response.jsonPath();
-        String userAgent = jsonPath.get("user_agent");
-       System.out.println("user-agent  "+ userAgent);
+//        String userAgent = jsonPath.get("user_agent");
+ //      System.out.println("user-agent  "+ userAgent);
         String platform = jsonPath.get("platform");
         String browser = jsonPath.get("browser");
         String device = jsonPath.get("device");
