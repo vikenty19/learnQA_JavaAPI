b628c22227af4bd23e8d5c4976b079292af50a00
vikenty19 <vikenty.plakhov@testpro.io>
2023-08-08 16:08:05 -0400

    restTraining

--- src/test/java/Homework3.java
+++ src/test/java/Homework3.java
@@ -16,79 +16,81 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class Homework3 {
     @ParameterizedTest
-    @ValueSource(strings = {"1","12345","12345678910123456","vasyavasyavasya","vasyavasyavasya1"} )
-    public void shortPhraseTest(String givenString){
+    @ValueSource(strings = {"1", "12345", "12345678910123456", "vasyavasyavasya", "vasyavasyavasya1"})
+    public void shortPhraseTest(String givenString) {
         int length = givenString.length();
-        if(length<=15){ givenString = "Name doesn't exist";}
+        if (length <= 15) {
+            givenString = "Name doesn't exist";
+        }
         String answer = (length > 15) ? givenString : "Name doesn't exist";
         System.out.println(answer);
-          assertEquals(givenString,answer,"Name doesn't exist");
+        assertEquals(givenString, answer, "Name doesn't exist");
 
     }
 
-     @Test
-      public void assertCookie(){
+    @Test
+    public void assertCookie() {
         Response response = RestAssured
                 .get("https://playground.learnqa.ru/api/homework_cookie")
                 .andReturn();
-        Map<String,String> responseCookie = response.getCookies();
-         System.out.println(responseCookie);
-         String cookie = response.getCookie("HomeWork");
-         assertEquals("hw_value",cookie);
-     }
+        Map<String, String> responseCookie = response.getCookies();
+        System.out.println(responseCookie);
+        String cookie = response.getCookie("HomeWork");
+        assertEquals("hw_value", cookie);
+    }
+
     @Test
-    public void assertHeader(){
+    public void assertHeader() {
         Response response = RestAssured
                 .get("https://playground.learnqa.ru/api/homework_header")
                 .andReturn();
 //        response.prettyPrint();
-      Headers responseHeader = response.getHeaders();
-   //     System.out.println(responseHeader);
-        String  header = response.jsonPath().getString("success");
+        Headers responseHeader = response.getHeaders();
+        //     System.out.println(responseHeader);
+        String header = response.jsonPath().getString("success");
         System.out.println(header);
-         assertTrue(responseHeader.hasHeaderWithName("success"));
-       assertEquals("!",header);
+        assertTrue(responseHeader.hasHeaderWithName("success"));
+        assertEquals("!", header);
     }
 
 
-   @ParameterizedTest
-   @ValueSource(strings = {"Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
-   "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
-   "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1",
-   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0",
-   "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"})
-    public void checkUserAgent(String name){
-        Map<String,String> headers = new HashMap<>();
-        headers.put("user-agent",name);
-        Response response =RestAssured
+    @ParameterizedTest
+    @ValueSource(strings = {"Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+            "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+            "Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0",
+            "Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1"})
+    public void checkUserAgent(String name) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("user-agent", name);
+        Response response = RestAssured
                 .given()
                 .headers(headers)
                 .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
                 .andReturn();
-  //      response.prettyPrint();
+        //      response.prettyPrint();
         JsonPath jsonPath = response.jsonPath();
-//        String userAgent = jsonPath.get("user_agent");
- //      System.out.println("user-agent  "+ userAgent);
+ //       String userAgent = jsonPath.get("user_agent");
+   //     System.out.println("user-agent  " + userAgent);
         String platform = jsonPath.get("platform");
         String browser = jsonPath.get("browser");
         String device = jsonPath.get("device");
-        if(platform.equalsIgnoreCase("Unknown")
+        if (platform.equalsIgnoreCase("Unknown")
                 || (browser.equalsIgnoreCase("Unknown"))
-                 || (device.equalsIgnoreCase("Unknown"))){
-            System.out.println(name);
-            if(platform.equalsIgnoreCase("Unknown")){
+                || (device.equalsIgnoreCase("Unknown"))) {
+            System.out.println("My system data is   " + name);
+            if (platform.equalsIgnoreCase("Unknown")) {
                 System.out.println("platform is unknown");
             }
-            if(browser.equalsIgnoreCase("Unknown")){
+            if (browser.equalsIgnoreCase("Unknown")) {
                 System.out.println("browser is Unknown");
             }
-            if(device.equalsIgnoreCase("Unknown")){
+            if (device.equalsIgnoreCase("Unknown")) {
                 System.out.println("device is Unknown");
             }
         }
 
-   }
-
+    }
 
 
 }
---
+++ src/test/java/RestTraining.java
@@ -0,0 +1,48 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.Assert;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+
+public class RestTraining {
+    @Test
+    public void restAssuredTest() {
+        given()
+                .when()
+                .get("https://restful-booker.herokuapp.com/ping")
+                .then()
+                .assertThat()
+                .statusCode(201);
+    }
+
+    @Test
+    public void getBookingIdsTests() {
+        Response response = RestAssured
+                .get("https://restful-booker.herokuapp.com/booking")
+                .andReturn();
+        response.prettyPrint();
+        Assert.assertEquals(response.getStatusCode(), 200);
+        List<Integer> bookingId = response.jsonPath().getList("bookingid");
+        Assert.assertFalse(bookingId.isEmpty());
+        //   System.out.println(bookingId);
+    }
+
+    @Test
+    public void getBookingName() {
+        Response response = RestAssured
+                .get("https://restful-booker.herokuapp.com/booking/1")
+                .andReturn();
+        response.prettyPrint();
+        Assert.assertEquals(response.getStatusCode(), 200);
+        String bookingFirstName = response.jsonPath().getString("firstname");
+        String bookingLastName = response.jsonPath().getString("lastname");
+        System.out.print(bookingFirstName +  "   last name   " + bookingLastName);
+        //   Assert.assertFalse(bookingId.isEmpty())
+   assertAll();
+
+    }
+}
