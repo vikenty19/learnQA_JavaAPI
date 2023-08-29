f969da34ad9d26c6940bfea073b6fece9c49a823
vikenty19 <vikenty.plakhov@testpro.io>
2023-08-29 15:19:22 -0400

    Update Polindrom.java

--- src/test/java/Polindrom.java
+++ src/test/java/Polindrom.java
@@ -1,39 +1,74 @@
 import org.apache.logging.log4j.core.util.JsonUtils;
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Polindrom {
+
     @Test
     public void getWordAsChar() {
         String string = "abc";
         char[] chars = string.toCharArray();
-        for(char char1: chars){
-        System.out.print(char1 + " ");}
+        for (char char1 : chars) {
+            System.out.print(char1 + " ");
+        }
         System.out.println(chars.length);
-        for(int i =chars.length-1;i >= 0;i --){
+        for (int i = chars.length - 1; i >= 0; i--) {
             System.out.print(chars[i] + " ");
         }
 
     }
 
 
+    @Test
+    public void checkWord() {
+        System.out.println(isPalindrome("Aaaaa"));
 
-        @Test
-        public void checkWord(){
-            System.out.println(isPalindrome("Aaaa1"));
+        List<Integer> number1 = new ArrayList<>();
+        number1.add(3);
+        number1.add(1);
+        number1.add(3);
+        deleteThree(number1);
+
+    }
+
+    String myStringReverse = "";
+
+    public boolean isPalindrome(String myString) {
+
+        for (int i = myString.length() - 1; i >= 0; i--) {
+            myStringReverse = myStringReverse + myString.charAt(i);
+
         }
-    String myStringReverse ;
-        public boolean isPalindrome(String myString) {
 
-            for (int i = myString.length() - 1; i >= 0; i--) {
-                myStringReverse = myStringReverse + myString.charAt(i);
-            }
-            if (myString.equalsIgnoreCase(myStringReverse)) {
-                return true;
-            } else {
-                return false;
-            }
 
+        Assert.assertTrue(myString.equalsIgnoreCase(myStringReverse));
+        System.out.println(myStringReverse + "  " + myString);
+        if (myString.equalsIgnoreCase(myStringReverse)) {
+            return true;
+        } else {
+            return false;
         }
 
     }
 
+
+
+    public void deleteThree(List<Integer> numbers) {
+
+
+
+       // for (Integer num : numbers) {
+        for(int i = 0; i < numbers.size(); i++){
+            if (numbers.get(i)== 3) {
+                numbers.remove(numbers.get(i));
+            }
+
+       }
+        System.out.println(numbers);
+    }
+
+
+}
