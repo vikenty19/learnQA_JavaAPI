343bc23e2e0a3b8c3862ba0a1d532466ff1892f3
vikenty19 <vikenty.plakhov@testpro.io>
2023-11-08 18:35:33 -0500

    Update HelloWorldTest.java

--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -18,7 +18,7 @@ public class HelloWorldTest {
                 .queryParams(params)
                 .get("https://playground.learnqa.ru/api/hello")
                 .jsonPath();
-     
+
         String answer = response.getString("answer");
 
         if (answer == null) {
@@ -57,5 +57,26 @@ public class HelloWorldTest {
                 .andReturn();
         response.prettyPrint();
 
+    }@Test
+    public void getWithParamsOrBody(){
+        Response response = RestAssured
+                .given()
+                .queryParam("\"param1\":\"value1\",\"param2\":\"value2\"")
+                .get("https://playground.learnqa.ru/api/check_type")
+                .andReturn();
+        response.print();
+
+    }
+    @Test
+    public void postWithParams(){
+        Map<String,Object> body = new HashMap<>();
+        body.put("param1","value1");
+        body.put("param2","value2");
+        Response response = RestAssured
+                .given()
+                .body(body)
+                .post("https://playground.learnqa.ru/api/check_type")
+                .andReturn();
+        response.print();
     }
 }
