ee915e62944cf82de4279b66d4eb300be8fb926e
vikenty19 <vikenty.plakhov@testpro.io>
2023-11-18 13:39:13 -0500

    Update BaseTest.java

--- src/test/java/lib/BaseTest.java
+++ src/test/java/lib/BaseTest.java
@@ -1,24 +1,29 @@
 package lib;
 
-import io.restassured.http.Header;
+
 import io.restassured.http.Headers;
 import io.restassured.response.Response;
 import org.junit.Before;
 
 import java.util.Map;
 
+import static org.hamcrest.Matchers.hasKey;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BaseTest {
-    protected String getHeader(Response response,String name) {
-       Headers headers = response.getHeaders();
+    protected String getHeader(Response Response,String name) {
+       Headers headers = Response.getHeaders();
        assertTrue(headers.hasHeaderWithName(name),"headers doesn't have header with name" + name);
        return headers.getValue(name);
     }
-    protected String getCookie(Response response,String name){
+    protected String getCookie(Response Response,String name){
 
-        Map<String,String> cookies = response.getCookies();
+        Map<String,String> cookies = Response.getCookies();
         assertTrue(cookies.containsKey(name),"cookies doesn't have cookies with name" + name);
         return cookies.get(name);
+    }
+    protected int getIntFromJson(Response Response,String name){
+       Response.then().assertThat().body("$",hasKey((name)));
+       return Response.jsonPath().getInt(name);
     }
 }
