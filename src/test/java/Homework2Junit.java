dbc48b09d438b297b2ce2d8a27132bd705d61b5e
vikenty19 <vikenty.plakhov@testpro.io>
2024-04-13 17:31:53 -0400

    @Parametrized Test homework 2

--- src/test/java/Homework2Junit.java
+++ src/test/java/Homework2Junit.java
@@ -1,18 +1,23 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Homework2Junit {
 
-    @Test
-    public void helloWorldWithoutName(){
-        JsonPath response = RestAssured
-                .get("https://playground.learnqa.ru/api/hello")
-                .jsonPath();
-        assertEquals("Hello, someone",response.getString("answer"),"Unexpected result");
-    }
+//    @Test
+//    public void helloWorldWithoutName(){
+//        JsonPath response = RestAssured
+//                .get("https://playground.learnqa.ru/api/hello")
+//                .jsonPath();
+//        assertEquals("Hello, someone",response.getString("answer"),"Unexpected result");
+//    }
     @Test
     public void helloWorldWithName(){
         String name = "Vasya";
@@ -22,5 +27,23 @@ public class Homework2Junit {
                 .get("https://playground.learnqa.ru/api/hello")
                 .jsonPath();
         assertEquals("Hello, " + name,response.getString("answer"),"Unexpected result");
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"","Vasya","John","<body></body>"})
+public void helloWorldWithParams(String name){
+        Map<String,String> queryParams = new HashMap<>();
+        if (name.length() > 0){
+            queryParams.put("name",name);
+
+        }
+        JsonPath response = RestAssured
+                .given()
+                .queryParams(queryParams)
+                .get("https://playground.learnqa.ru/api/hello")
+                .jsonPath();
+        String answer = response.getString("answer");
+        String expectedAnswer = (name.length()>0)? name:"someone";
+        System.out.println(expectedAnswer);
+        assertEquals("Hello, " + expectedAnswer,answer,"Unexpected answer");
     }
 }
